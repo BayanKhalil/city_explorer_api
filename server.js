228f2d30758a9763126b5bd4e57b6f4c4c87294b
Bayan Khalil <bayan.khalil92@yahoo.com>
2021-03-29 05:58:23 +0300

    lab 06

--- server.js
+++ server.js
@@ -31,13 +31,12 @@ function handleLocation(request, response) {
     //     latitude: getLocation[0].lat,
     //     longitude: getLocation[0].lon
     const data = arrData[0];
-
-    new Location(request.query.city, data.display_name, data.lat, data.lon);
+    let obj=new Location(request.query.city, data.display_name, data.lat, data.lon);
+    response.send(obj);
 
 
     };
     response.send("hello there!!!!");
-    response.send(obj);
 // }
 
 function Location(name, location, latitude, longitude) {
@@ -46,6 +45,27 @@ function Location(name, location, latitude, longitude) {
         this.latitude = latitude,
         this.longitude = longitude
 }
+
+function Weather(description, valid_date) {
+    this.forecast = description,
+     this.time = valid_date;
+
+}
+
+function handleWeather(request, response){
+    const objData = require('./data/weather.json');
+    const weatherData = objData.data;
+    const returnedData = [];
+    console.log(request.query)
+    weatherData.forEach(a => {
+        returnedData.push(new Weather(a.weather.description, a.valid_date));
+    });
+    response.send(returnedData);
+
+
+}
+
+
 
 
 app.listen(PORT, ()=> console.log(`App is running on Server on port: ${PORT}`));
